2ca19ab397dfb7254b6d722189e989c3efb2afb4
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-08-06 09:46:16 +0100

    Relative Strength Index vs Close price example

---
+++ rsi_vs_close.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+import matplotlib.pyplot as plt
+
+api_key = 'RNZPXZ6Q9FEFMEHM'
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data_ts, meta_data_ts = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+
+periods = 14
+
+ti = TechIndicators(key=api_key, output_format='pandas')
+data_ti, meta_data_ti = ti.get_rsi(symbol='MSFT', interval='1min',
+                         time_period=periods, series_type='close')
+
+
+df1 = data_ti
+df2 = data_ts['4. close'].iloc[periods::]
+
+print(df1)
+df1.index = df2.index
+print(df2)
+
+fig, ax1 = plt.subplots()
+ax1.plot(df1, 'b-')
+ax2 = ax1.twinx()
+ax2.plot(df2, 'r.')
+plt.title('RSI vs Close Price')
+plt.show()
