7e4de2760ef33450cb091daa7798109d43a3f9f9
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-08-06 14:36:08 +0100

    Remove Redundant Code

--- rsi_vs_close.py
+++ rsi_vs_close.py
@@ -10,7 +10,6 @@ data_ts, meta_data_ts = ts.get_intraday(symbol='MSFT',interval='1min', outputsiz
 
 periods = 14
 
-ti = TechIndicators(key=api_key, output_format='pandas')
 data_ti, meta_data_ti = ti.get_rsi(symbol='MSFT', interval='1min',
                          time_period=periods, series_type='close')
 
